a165f916e82f551e5f14b6f03651a94cf4c701dc
Toby8138 <lauwaiyin26@gmail.com>
2025-12-06 02:06:05 +0100

    Implement Kahan's algorithm alongside Heron's formula
    Added Kahan's algorithm for numerical stability in area calculation.

--- Area_of_a_triangle.jl
+++ Area_of_a_triangle.jl
@@ -1,10 +1,12 @@
 using LinearAlgebra
 
+# Standard Heron's Formula
 function Heron(a, b, c)
     s = 1/2*(a + b + c)
     return s*(s-a)*(s-b)*(s-c)
 end
 
+# Kahan's numerical stable algorithm 
 Kahan(a, b, c) = 1/16*(a + (b + c)) * (c - (a - b)) * (c + (a - b)) * (a + (b - c))
 
 
@@ -12,6 +14,8 @@ Kahan(a, b, c) = 1/16*(a + (b + c)) * (c - (a - b)) * (c + (a - b)) * (a + (b - 
 x = 3
 y = 4
 z = 5
+
+# The two calculated areas are the same
 println(Heron(x, y, z))
 println(Kahan(x, y, z))
 
@@ -20,5 +24,6 @@ a = 10000000000
 b = 10000000000
 c = 0.00000001
 
+# The two calculated areas are completely different
 println(Heron(a, b, c))
 println(Kahan(a, b, c))
